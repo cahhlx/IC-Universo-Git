b19c3b3aec98c9a4142dd51be94c098289964263
cahhlx <carolmoralesleitao07@gmail.com>
2025-07-03 23:31:37 -0300

    adições e testes das cameras - part1

--- IC - Universo - Git/Assets/Scripts/MovimentoConstante.cs
+++ IC - Universo - Git/Assets/Scripts/MovimentoConstante.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
-public class MovimentoConstanteY : MonoBehaviour
+public class MovimentoConstante : MonoBehaviour
 {
     public float velocidade = 5f;
 
     void Update()
     {
-        // Move o objeto para cima (eixo Y) a uma velocidade constante
+        // Move o objeto para cima (eixo Y) com uma velocidade constante
         transform.Translate(Vector3.up * velocidade * Time.deltaTime);
     }
 }
